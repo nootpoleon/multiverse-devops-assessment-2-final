5bee0b7d000289f3d5276010797180f0744361de
sbailey3103 <>
2023-03-24 17:15:46 +0000

    coded ticket 5

--- input/__init__.py
+++ input/__init__.py
@@ -39,3 +39,16 @@ def capitalise_username(file):
   return capitalise
 
 capitaliseddata = capitalise_username(file)
+
+def ans3valid(file):
+  noblanks =[]
+  for i in capitaliseddata:
+      if len([x for x in i if x != '']) == 6: #removes any line nan entries
+          noblanks.append(i)
+  
+  noblanks = np.array(noblanks)
+  
+  valid_ans3 = [x for x in noblanks[1:] if int(x[5]) > 0 and int(x[5])<11]
+  valid_ans3.insert(0, noblanks[0])
+  valid_ans3 = np.array(valid_ans3)
+  return valid_ans3
