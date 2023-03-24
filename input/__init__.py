b3c8a248ca1f3ec6c63aa7fb0e91925c4bf6ae09
sbailey3103 <>
2023-03-24 15:54:45 +0000

    coded ticket2

--- input/__init__.py
+++ input/__init__.py
@@ -10,3 +10,12 @@ def get_csv(file):
   return data
 
 data = get_csv('results.csv')
+
+def drop_duplicate(file):
+  no_duplicates = []
+  for x in data:
+    if x not in no_duplicates:
+      no_duplicates.append(x)
+  return no_duplicates
+
+datanodupl = drop_duplicate(file)
