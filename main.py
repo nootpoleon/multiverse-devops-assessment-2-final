1b696dc345f464c8f9b87e3c8935d39f785b957d
sbailey3103 <>
2023-03-24 17:25:04 +0000

    Added App Input

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pytest
+from input import get_csv
+from input import drop_duplicate
+from input import drop_null
+from input import capitalise_username
+from input import ans3valid
+from input import exportcsv
+from input import finalresult
+
+
+file = open('results.csv')
+data = get_csv(file)
+data = drop_duplicate(data)
+data = drop_null(data)
+data = capitalise_username(data)
+data = ans3valid(data)
+data = exportcsv(data)
+data = finalresult(data)
